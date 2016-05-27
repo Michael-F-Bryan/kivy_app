3d2d0325038e14bd9c5df0d0e0e735db19ca95cc
Michael Bryan <michaelfbryan@gmail.com>
2016-05-28 07:56:48 +0800

    Receiving weather data and printing it to screen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from kivy.app import App
-from kivy.factory import Factory
-from kivy.properties import ObjectProperty, ListProperty
+from kivy.properties import (ObjectProperty, ListProperty, StringProperty, 
+        NumericProperty)
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.network.urlrequest import UrlRequest
@@ -10,7 +10,28 @@ from kivy.uix.listview import ListItemButton
 
 Location = namedtuple('Location', ('city', 'country'))
 API_KEY = 'cf267c34f8652831dc9b714fb0576fbb'
+base_url = 'http://api.openweathermap.org/data/2.5/' 
 
+
+class CurrentWeather(BoxLayout):
+    location = ListProperty(['Perth', 'AU'])
+    conditions = StringProperty()
+    temp = NumericProperty()
+    temp_min = NumericProperty()
+    temp_max = NumericProperty()
+
+    def update_weather(self):
+        weather_template = base_url + 'weather?q={},{}&units=metric&APPID={APPID}'
+        url = weather_template.format(*self.location, APPID=API_KEY)
+        UrlRequest(url, self.weather_retrieved)
+
+    def weather_retrieved(self, request, data):
+        data = json.loads(data.decode()) if not isinstance(data, dict) else data
+        self.conditions = data['weather'][0]['description']
+        self.temp = data['main']['temp']
+        self.temp_min = data['main']['temp_min']
+        self.temp_max = data['main']['temp_max']
+        
 
 class WeatherRoot(BoxLayout):
     """The root widget for the app"""
@@ -19,14 +40,13 @@ class WeatherRoot(BoxLayout):
     def show_current_weather(self, location=None):
         self.clear_widgets()
 
-        if location is None and self.current_weather is None:
-            # Some convenient default value
-            location =['Perth', 'AU']
+        if self.current_weather is None:
+            self.current_weather = CurrentWeather()
 
         if location is not None:
-            self.current_weather = Factory.CurrentWeather()
             self.current_weather.location = location
 
+        self.current_weather.update_weather()
         self.add_widget(self.current_weather)
 
     def show_add_location_form(self):
@@ -50,7 +70,6 @@ class AddLocationForm(BoxLayout):
         if not self.search_input.text:
             return
 
-        base_url = 'http://api.openweathermap.org/data/2.5/' 
         search_template = base_url + 'find?q={query}&type=like&APPID={API_KEY}'
         url = search_template.format(query=self.search_input.text,
                 API_KEY=API_KEY)
