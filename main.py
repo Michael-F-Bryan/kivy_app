1c4e35476f769e20d77c061a4e5b800c22f173b1
Michael Bryan <michaelfbryan@gmail.com>
2016-05-28 06:48:32 +0800

    When a city is clicked, it'll change the page

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from kivy.app import App
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.network.urlrequest import UrlRequest
-
+from kivy.uix.listview import ListItemButton
 
 
 API_KEY = 'cf267c34f8652831dc9b714fb0576fbb'
 
 
+class WeatherRoot(BoxLayout):
+    """The root widget for the app"""
+
+class LocationButton(ListItemButton):
+    """A subclass of ListItemButton so we can give it event handlers"""
+
 
 class AddLocationForm(BoxLayout):
+    """A form for adding new locations for the app"""
     search_input = ObjectProperty()
     search_results = ObjectProperty()
 
     def search_location(self):
+        """Send off a request to the API"""
         if not self.search_input.text:
             return
 
@@ -25,13 +34,22 @@ class AddLocationForm(BoxLayout):
         request = UrlRequest(url, self.found_location)
 
     def found_location(self, request, data):
+        """The callback executed when we get a response back"""
         cities = ['{} ({})'.format(d['name'], d['sys']['country']) 
                 for d in data['list']]
         self.search_results.item_strings = cities
 
+        self.search_results.adapter.data.clear()
+        self.search_results.adapter.data.extend(cities)
+        self.search_results._trigger_reset_populate()
+
 
 class WeatherApp(App):
-    pass
+    """The app itself"""
+
+    def show_current_weather(self, location):
+        self.clear_widgets()
+        self.add_widget(Label(text=location))
 
 
 if __name__ == "__main__":
